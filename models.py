7ee813c5653c9e0f1bb71417e9c5a44152352e23
DmitryAsl <aslamovdima10@yandex.ru>
2024-12-29 01:02:36 +0300

    add methods and tests

--- models.py
+++ models.py
@@ -1,3 +1,7 @@
+from typing import Dict
+from dataclasses import dataclass
+
+
 class Product:
     """
     Класс продукта
@@ -14,19 +18,15 @@ class Product:
         self.quantity = quantity
 
     def check_quantity(self, quantity) -> bool:
-        """
-        TODO Верните True если количество продукта больше или равно запрашиваемому
-            и False в обратном случае
-        """
-        raise NotImplementedError
+        return self.quantity >= quantity
 
     def buy(self, quantity):
-        """
-        TODO реализуйте метод покупки
-            Проверьте количество продукта используя метод check_quantity
-            Если продуктов не хватает, то выбросите исключение ValueError
-        """
-        raise NotImplementedError
+        if quantity <= 0:
+            raise ValueError('Количество товаров для продажи может быть только положительным числом')
+        if self.check_quantity(quantity):
+            self.quantity -= quantity
+        else:
+            raise ValueError('Количества товара на остатках недостаточно для продажи')
 
     def __hash__(self):
         return hash(self.name + self.description)
@@ -35,11 +35,10 @@ class Product:
 class Cart:
     """
     Класс корзины. В нем хранятся продукты, которые пользователь хочет купить.
-    TODO реализуйте все методы класса
     """
 
     # Словарь продуктов и их количество в корзине
-    products: dict[Product, int]
+    products: Dict[Product, int]
 
     def __init__(self):
         # По-умолчанию корзина пустая
@@ -50,7 +49,12 @@ class Cart:
         Метод добавления продукта в корзину.
         Если продукт уже есть в корзине, то увеличиваем количество
         """
-        raise NotImplementedError
+        if buy_count <= 0:
+            raise ValueError('Количество товара должно быть положительным числом')
+        if product in self.products:
+            self.products[product] += buy_count
+        else:
+            self.products[product] = buy_count
 
     def remove_product(self, product: Product, remove_count=None):
         """
@@ -58,13 +62,27 @@ class Cart:
         Если remove_count не передан, то удаляется вся позиция
         Если remove_count больше, чем количество продуктов в позиции, то удаляется вся позиция
         """
-        raise NotImplementedError
+        # если продукта нет в корзине, то удалять нечего
+        if product not in self.products:
+            return
+        if remove_count is None:
+            self.products.pop(product)
+        else:
+            if remove_count <= 0:
+                raise ValueError('Количество товара для удаления должно быть положительным числом')
+            if self.products[product] <= remove_count:
+                self.products.pop(product)
+            else:
+                self.products[product] -= remove_count
 
     def clear(self):
-        raise NotImplementedError
+        self.products = {}
 
     def get_total_price(self) -> float:
-        raise NotImplementedError
+        total_price = 0
+        for product, count in self.products.items():
+            total_price += product.price * count
+        return total_price
 
     def buy(self):
         """
@@ -72,4 +90,14 @@ class Cart:
         Учтите, что товаров может не хватать на складе.
         В этом случае нужно выбросить исключение ValueError
         """
-        raise NotImplementedError
+        if not self.products:
+            raise ValueError("Корзина пуста, нет товаров для продажи")
+
+        for product, count in self.products.items():
+            if not product.check_quantity(count):
+                raise ValueError(
+                    f"Количество товара {product.name} не хватает на складе. Остаток на складе {product.quantity}")
+
+        for product, count in self.products.items():
+            product.buy(count)
+        self.clear()
--- test_shop.py
+++ test_shop.py
@@ -1,14 +1,27 @@
-"""
-Протестируйте классы из модуля homework/models.py
-"""
 import pytest
 
 from models import Product
+from models import Cart
 
 
 @pytest.fixture
 def product():
     return Product("book", 100, "This is a book", 1000)
+
+
+@pytest.fixture
+def cart():
+    return Cart()
+
+
+@pytest.fixture
+def water():
+    return Product("Water good", 100, "This is a good water", 100)
+
+
+@pytest.fixture
+def bread():
+    return Product("Kybai", 33.3, 'Bread is head', 38)
 
 
 class TestProducts:
@@ -18,23 +31,112 @@ class TestProducts:
     """
 
     def test_product_check_quantity(self, product):
-        # TODO напишите проверки на метод check_quantity
-        pass
+        assert product.check_quantity(1000)
+        assert product.check_quantity(999)
+        assert not product.check_quantity(1001)
 
     def test_product_buy(self, product):
-        # TODO напишите проверки на метод buy
-        pass
+        product.buy(998)
+        assert product.quantity == 2
+
+        product.buy(2)
+        assert product.quantity == 0
 
     def test_product_buy_more_than_available(self, product):
-        # TODO напишите проверки на метод buy,
-        #  которые ожидают ошибку ValueError при попытке купить больше, чем есть в наличии
-        pass
+        with pytest.raises(ValueError, match='Количества товара на остатках недостаточно для продажи'):
+            product.buy(1001)
+
+    def test_product_buy_incorrect_quantity(self, product):
+        with pytest.raises(ValueError, match='Количество товаров для продажи может быть только положительным числом'):
+            product.buy(-1)
+        with pytest.raises(ValueError, match='Количество товаров для продажи может быть только положительным числом'):
+            product.buy(0)
 
 
 class TestCart:
-    """
-    TODO Напишите тесты на методы класса Cart
-        На каждый метод у вас должен получиться отдельный тест
-        На некоторые методы у вас может быть несколько тестов.
-        Например, негативные тесты, ожидающие ошибку (используйте pytest.raises, чтобы проверить это)
-    """
+
+    def test_add_product_in_cart(self, cart, water, bread):
+        cart.add_product(water)
+        assert len(cart.products) == 1 and cart.products[water] == 1
+
+        cart.add_product(water)
+        assert len(cart.products) == 1 and cart.products[water] == 2
+
+        cart.add_product(bread, 39)
+        assert len(cart.products) == 2 and cart.products[bread] == 39
+
+    def test_add_product_incorrect_quantity(self, cart, water):
+        with pytest.raises(ValueError, match="Количество товара должно быть положительным числом"):
+            cart.add_product(water, -1)
+        with pytest.raises(ValueError, match="Количество товара должно быть положительным числом"):
+            cart.add_product(water, 0)
+
+    def test_remove_product_from_cart(self, cart, water, bread):
+        cart.add_product(water, 3)
+        cart.remove_product(water, 1)
+        assert len(cart.products) == 1 and cart.products[water] == 2
+
+        cart.remove_product(water)
+        assert len(cart.products) == 0
+
+        cart.add_product(water)
+        cart.add_product(bread)
+        cart.remove_product(water, 100000)
+        assert len(cart.products) == 1 and cart.products[bread] == 1
+
+        cart.remove_product(bread, 1)
+        assert len(cart.products) == 0
+
+        cart.add_product(water)
+        cart.remove_product(bread)
+        assert len(cart.products) == 1
+
+    def test_remove_product_incorrect_quantity(self, cart, water):
+        cart.add_product(water)
+        with pytest.raises(ValueError, match='Количество товара для удаления должно быть положительным числом'):
+            cart.remove_product(water, 0)
+        with pytest.raises(ValueError, match="Количество товара для удаления должно быть положительным числом"):
+            cart.remove_product(water, -1)
+
+    def test_clear_cart(self, cart, water, bread):
+        cart.clear()
+        assert len(cart.products) == 0
+
+        cart.add_product(water)
+        cart.add_product(bread)
+        cart.clear()
+        assert len(cart.products) == 0
+
+    def test_total_price_by_cart(self, cart, water, bread):
+        assert cart.get_total_price() == 0
+
+        cart.add_product(water, 3)
+        cart.add_product(bread, 1)
+        assert cart.get_total_price() == pytest.approx(333.3)
+
+    def test_buy_products(self, cart, water, bread):
+        cart.add_product(water)
+        cart.add_product(bread, 38)
+        cart.buy()
+        assert len(cart.products) == 0
+        assert water.quantity == 99 and bread.quantity == 0
+
+        cart.add_product(water)
+        cart.add_product(water, 98)
+        cart.buy()
+        assert len(cart.products) == 0
+        assert water.quantity == 0
+
+    def test_buy_empty_cart(self, cart):
+        with pytest.raises(ValueError, match="Корзина пуста, нет товаров для продажи"):
+            cart.buy()
+
+    def test_buy_product_more_remains(self, cart, water, bread):
+        cart.add_product(bread, 10)
+        cart.add_product(water, 101)
+        with pytest.raises(ValueError,
+                           match=f"Количество товара {water.name} не хватает на складе. Остаток на складе {water.quantity}"):
+            cart.buy()
+
+        assert len(cart.products) == 2
+        assert bread.quantity == 38 and water.quantity == 100
